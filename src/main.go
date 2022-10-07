074d5136f43f795e70efc0967c5a78a8fdb58d66
gogo.afuzzz <gogo.afuzzz@gmail.com>
2022-10-07 11:36:42 +0800

    Add gin-gonic http web framework

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(":8000")
+}
