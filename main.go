b8e31f4b6f6ce946f35fefafd9e01233dad61409
gogo.afuzzz <gogo.afuzzz@gmail.com>
2022-10-08 04:45:50 +0800

    自訂義 Middleware - Gin Http 日誌 & BasicAuth 驗證使用者

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gogoafuzzz/blog-go/controller"
+	"github.com/gogoafuzzz/blog-go/middleware"
 	"github.com/gogoafuzzz/blog-go/service"
 )
 
@@ -13,9 +16,21 @@ var (
 	articleController controller.ArticleController = controller.New(articleService)
 )
 
+var fp *os.File
+
+// 將 gin 請求記錄寫到日誌中
+func setupLogOutput() {
+	fp, _ = os.Create("http.log")
+	gin.DefaultWriter = io.MultiWriter(fp, os.Stdout)
+}
+
 func main() {
 
-	router := gin.Default()
+	setupLogOutput()
+	defer fp.Close()
+
+	router := gin.New()
+	router.Use(gin.Recovery(), middleware.Logger(), middleware.BasicAuth())
 
 	articles := router.Group("articles")
 	{
---
+++ middleware/logger.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 自訂義 gin logger 顯示方式
+func Logger() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] %s %s %d %s \n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC822),
+			param.Method,
+			param.Path,
+			param.StatusCode,
+			param.Latency,
+		)
+	})
+}
